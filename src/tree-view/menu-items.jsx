e72f2562a801b2179b96bbd266093fbe91f02d99
Aziz <azizatamuradov1994@gmail.com>
2025-07-12 08:55:51 -0400

    components created

---
+++ src/tree-view/menu-items.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import MenuList from './menu-list'
+
+
+const MenuItem = ({item}) => {
+  return (
+    <div>Menu item</div>
+  )
+}
+
+export default MenuItem
---
+++ src/tree-view/menu-list.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MenuItem from './menu-items'
+
+
+const MenuList = ({list = []}) => {
+  return (
+      <div className='menu-list-container'>
+          {
+              list && list.length ? 
+              list.map((listItem) => <MenuItem item={listItem} />)
+              : null
+          }
+    </div>
+  )
+}
+
+export default MenuList
